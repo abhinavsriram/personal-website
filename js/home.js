6264d92291dd5e69e846cce1fd2594bbe5119779
Abhinav Sriram <sriramabhinav2112@gmail.com>
2020-08-09 01:57:52 +0530

    reorganized file structure

---
+++ js/home.js
@@ -0,0 +1,23 @@
+/*–––––––––––––––––––––––––JAVASCRIPT ANIMATION OF BURGER MENU–––––––––––––––––––––––––*/
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // toggle nav
+        nav.classList.toggle('nav-active');
+        // animate nav
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards';
+            }
+        });
+        burger.classList.toggle('toggle');
+        });
+
+}
+
+navSlide();
